4b27af159cb3963a09472c2eef8b81dabd2ea064
Josuesp1620 <josuesp1620@gmail.com>
2024-04-19 20:45:10 +0200

    Update Name Routes

--- src/core/routes/v1/const.routes.ts
+++ src/core/routes/v1/const.routes.ts
@@ -1,6 +1,6 @@
 export const ROUTE_VERSION = '/api/v1'
 
-const APPLICATION_API : string = '/application-api'
+const APPLICATION_API : string = '/gateway-endpoint'
 const APPLICATION_API_CREATE : string = '/create'
 
 export const ROUTES_APPLICATION_API = {
@@ -8,7 +8,7 @@ export const ROUTES_APPLICATION_API = {
     APPLICATION_API_CREATE,
 }
 
-const ENDPOINT_API : string = '/endpoint-api'
+const ENDPOINT_API : string = '/gateway-endpoint'
 const ENDPOINT_API_CREATE : string = '/create'
 export const ROUTES_ENDPOINT_API = {
     ENDPOINT_API,
